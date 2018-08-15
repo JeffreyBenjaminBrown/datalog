cc3409211bc5ea219c7d6918bba8037231ba1983
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-08-14 21:09:50 -0500

    comments, examples

--- jbb/rslt.hs
+++ jbb/rslt.hs
@@ -38,6 +38,13 @@ instance Show a => Show (Vect n a) where
 type Label = String
 type Arity = SNat
 
+-- | Relationships come in various flavors. For instance,
+-- "some _ go _ sometimes" is a binary relationship, and like all binnary
+-- relationships, it has a 3-part label.
+-- PITFALL: Since Arity uses SNat and Vect uses Nat, the arity
+-- is only one less than the vect length (not 2 as might appear below).
+-- PITFALL: The least value of `n` possible is 'Z, which confusingly
+-- corresponds to an arity of 1, not 0 (again because Arity = SNat).
 type Flavor n = (Arity n, Vect ('S ('S n)) String)
 
 ar = (one, "a" >- "b" >- VNil) :: Flavor 'Z
@@ -45,9 +52,15 @@ ar = (one, "a" >- "b" >- VNil) :: Flavor 'Z
 data HypergraphNode where
   Atom :: Label -> HypergraphNode
   RelationshipFlavor :: Flavor n -> HypergraphNode
-  Relationship :: Flavor n -> Vect ('S n) HypergraphNode -> HypergraphNode
+  Relationship :: forall n.
+    Flavor n -> Vect ('S n) HypergraphNode -> HypergraphNode
+
+bar :: HypergraphNode
+bar = Relationship
+      (one, "The" >- "is cool." >- VNil)
+      (Atom "shark" >- VNil)
 
 foo :: HypergraphNode
 foo = Relationship
-      (two, "The" >- "comes before the" >- "" >- VNil)
-      (Atom "a" >- Atom "b" >- VNil)
+      (two, "The" >- "goes before the" >- "." >- VNil)
+      (Atom "horse" >- Atom "cart" >- VNil)
